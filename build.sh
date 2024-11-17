c55f52334d4e2c4ede83525df4c02c30c3e25f81
dougy147 <16362303+dougy147@users.noreply.github.com>
2024-11-17 21:57:38 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,10 @@ set -xe
 
 ocamlfind ocamlopt -I ./src -o ./src/parseum.native ./src/parseum.ml
 ocamlfind ocamlc   -I ./src -o ./src/parseum.byte   ./src/parseum.ml
+
+if [[ -n $1 && $1 == "example" ]]; then
+    pushd ./examples
+    ocamlfind ocamlopt -I ../src -o bnf.native ../src/parseum.ml bnf.ml
+    ocamlfind ocamlc   -I ../src -o bnf.byte   ../src/parseum.ml bnf.ml
+    popd
+fi
--- examples/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-ocamlfind ocamlopt -I ../src -o bnf.native ../src/parseum.ml bnf.ml
-ocamlfind ocamlc   -I ../src -o bnf.byte   ../src/parseum.ml bnf.ml
