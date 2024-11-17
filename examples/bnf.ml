a8c3721ada66ee43510eda0c91b65ee451caeb45
dougy147 <16362303+dougy147@users.noreply.github.com>
2024-11-17 21:50:03 +0100

    Update example bnf.ml

--- examples/bnf.ml
+++ examples/bnf.ml
@@ -1,22 +1,40 @@
 open Parseum
 
-let opt_spaces = many (char " ")
-let s = opt_spaces
+type terminal_t = string
+type non_terminal_t = string
+type subexpr_t = string list
+type expr_t = subexpr_t list
+type rule_t = non_terminal_t * (expr_t list)
+type grammar_t = rule_t list
 
-let non_terminal =
-    char "<" *> word <* char ">"
+let ws: string Parseum.parser =
+    top_result (many (char " "))
 
-let terminal =
-    char "\"" *> word <* char "\""
+let non_terminal: non_terminal_t Parseum.parser =
+    top_result (ws +> char "<" +> (many1 alnum) <+ char ">" <+ ws)
 
-let rule_name = non_terminal
+let rewrite_symbol: string Parseum.parser =
+    top_result (ws +> str "::=" <+ ws)
 
-let subexp = many (s *> (non_terminal ++ terminal) <* s)
+let terminal: terminal_t Parseum.parser =
+    top_result (ws +> (optional (char "\"")) +> many1 alnum <+ (optional (char "\"")) <+ ws)
 
-let expression = many (subexp <* char "|") <*> subexp
+let subexpr =
+    top_result (ws +> many1_list ( terminal <|> non_terminal ) <+ ws)
+
+let alternative =
+    ws +> char "|" <+ ws
+
+let expr =
+    top_result (many_list (subexpr <+ alternative) <+> fun x -> subexpr <+> fun xs -> result (x @ [xs]))
 
 let rule =
-    rule_name <* s *> prefix "::=" <* s <*> expression
+    top_result (non_terminal <+> fun identifier ->
+        rewrite_symbol <+> fun rewrite_symbol ->
+            expr <+> fun expression ->
+                result (identifier,expression))
 
 let grammar =
-    many (rule <* prefix "\n") <*> rule
+    top_result (many_list (rule <+ str "\n")) <+> fun x ->
+        rule <+> fun xs ->
+            result (x @ [xs])
